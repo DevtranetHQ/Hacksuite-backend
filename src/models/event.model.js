28be5edc140f4cc7a63bbf6a628f1c4b1574fe09
Eniola Osabiya <eosabiya@gmail.com>
2022-02-21 01:25:19 +0100

    feat: event schema updated

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
-     name: String
-});
+const eventSchema = new Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
 
-
-module.exports = mongoose.model("event", eventSchema)
+module.exports = mongoose.model("event", eventSchema);
